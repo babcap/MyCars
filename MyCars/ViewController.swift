6881ccc2c214341821e0fa5337b7cad0d67a207a
Arthur <takerarthur@gmail.com>
2020-04-14 20:33:08 +0300

    Get data from file

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -34,10 +34,83 @@ class ViewController: UIViewController {
         
     }
     
+//Check app launching with User Defaults
+    private func checkLaunchingAndGetDataIfNeeded() {
+        let userDefaults = UserDefaults.standard
+        let appLaunching = userDefaults.object(forKey: "launching") as? Bool
+        
+        guard let _ = appLaunching else {
+            getDataFromFile()
+            userDefaults.set(true, forKey: "launching")
+            return
+        }
+    }
+    
+//Check app launching with files in core data
+/*
+    private func checkLaunchingWithFilesInCoreData() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        
+        do {
+            records = try context.count(for: fetchRequest)
+            print("Is Data there already?")
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        guard records == 0 else { return }
+        getDataFromFile()
+    }
+*/
+    
+    private func getDataFromFile() {
+
+        print("getDataFromFile")
+        guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
+        let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+        
+        for dictionary in dataArray {
+            let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+            let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
+            
+            let carDictionary = dictionary as! [String : AnyObject]
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? Date
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoice = carDictionary["myChoice"] as! Bool
+            
+            let imageName = carDictionary["imageName"] as? String
+            guard let image = UIImage(named: imageName!),
+                let imageData = image.pngData() else { return }
+            car.imageData = imageData
+            
+            if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
+                car.tintColor = getColor(colorDictionary: colorDictionary)
+            }
+        }
+    }
+    
+    private func getColor(colorDictionary: [String : Float]) -> UIColor {
+        guard let red = colorDictionary["red"],
+        let green = colorDictionary["green"],
+        let blue = colorDictionary["blue"] else { return UIColor() }
+        return UIColor(displayP3Red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        checkLaunchingAndGetDataIfNeeded()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
     
 }
