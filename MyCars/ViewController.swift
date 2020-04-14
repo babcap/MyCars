da59c92daac0076acee39b3db4a0e32b67db332a
Arthur <takerarthur@gmail.com>
2020-04-14 22:05:47 +0300

    Insert data to UI

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     
     var context: NSManagedObjectContext!
     
+    lazy var dateFormater: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var markLabel: UILabel!
     @IBOutlet weak var modelLabel: UILabel!
@@ -34,43 +41,37 @@ class ViewController: UIViewController {
         
     }
     
-//Check app launching with User Defaults
-    private func checkLaunchingAndGetDataIfNeeded() {
-        let userDefaults = UserDefaults.standard
-        let appLaunching = userDefaults.object(forKey: "launching") as? Bool
+    private func insertDataFrom(selectedCar car: Car) {
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timesDriven)"
         
-        guard let _ = appLaunching else {
-            getDataFromFile()
-            userDefaults.set(true, forKey: "launching")
-            return
-        }
+        lastTimeStartedLabel.text = "Last day started: \(dateFormater.string(from: car.lastStarted!))"
+        segmentedControl.tintColor = car.tintColor as? UIColor
     }
-    
-//Check app launching with files in core data
-/*
-    private func checkLaunchingWithFilesInCoreData() {
+     
+    private func getDataFromFile() {
         let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
         fetchRequest.predicate = NSPredicate(format: "mark != nil")
         
         var records = 0
         
         do {
-            records = try context.count(for: fetchRequest)
-            print("Is Data there already?")
+        records = try context.count(for: fetchRequest)
+        print("Is Data there already?")
         } catch let error as NSError {
-            print(error.localizedDescription)
+        print(error.localizedDescription)
         }
         
         guard records == 0 else { return }
-        getDataFromFile()
-    }
-*/
-    
-    private func getDataFromFile() {
-
+        
         print("getDataFromFile")
+        
         guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
-        let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+            let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
         
         for dictionary in dataArray {
             let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
@@ -97,20 +98,28 @@ class ViewController: UIViewController {
     
     private func getColor(colorDictionary: [String : Float]) -> UIColor {
         guard let red = colorDictionary["red"],
-        let green = colorDictionary["green"],
-        let blue = colorDictionary["blue"] else { return UIColor() }
+            let green = colorDictionary["green"],
+            let blue = colorDictionary["blue"] else { return UIColor() }
         return UIColor(displayP3Red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1.0)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        checkLaunchingAndGetDataIfNeeded()
+        getDataFromFile()
         
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "mark == %@", mark!)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            let car = results.first
+            insertDataFrom(selectedCar: car!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
     }
     
 }
